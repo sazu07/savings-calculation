e3ea3117952936482ae0f39f72e8c559339bc2dd
sazu07 <sazushazahn0@gmail.com>
2022-10-30 16:05:27 +0600

    script validation

--- js/script.js
+++ js/script.js
@@ -1,16 +1,29 @@
 
 function addExpance(){
-    const income= document.getElementById('income').value;
-    const totalExpance=document.getElementById('total-expense');
+   
+    
     const food=document.getElementById('food').value;
     const rent= document.getElementById('rent').value;
     const cloth=document.getElementById('clothes').value;
+    if(food<0 || food=='' ||  rent<0||rent==''|| cloth<0 || cloth==''){
+        alert('input must be a posivet value')
+        return;
+    }
+    
+    const income= document.getElementById('income').value;
+    const totalExpance=document.getElementById('total-expense');
     const expances= parseFloat(food)+parseFloat(rent)+parseFloat(cloth);
+    if(income<expances){
+        alert('Income must be greater than expense');
+        return;
+    }
     totalExpance.innerText=expances;
+   
     const balanceField= document.getElementById('balance');
     const balance= income-expances;
     balanceField.innerText=balance
-  
+   
+    
 }
 
 
@@ -26,13 +39,11 @@ function calculateSavings(){
   const remainBalance= balanceAmount-saveBalance
   const fainalBalance=document.querySelector(".remaining-balance");
   fainalBalance.innerText=remainBalance;
-  
- 
- 
- 
-
 
 }
+document.getElementById('calculate-Savings').addEventListener('click', function(){
+    calculateSavings();
+})
 
 
 
